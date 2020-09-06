99f09882666985596d860530799d09103fceb046
floreez237 <florianlowe12@gmail.com>
2020-09-06 06:05:10 +0100

    Initial commit

---
+++ src/main/java/com/florian/reactivewebservices/ReactiveWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.florian.reactivewebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWebservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveWebservicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/florian/reactivewebservices/ReactiveWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florian.reactivewebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveWebservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
